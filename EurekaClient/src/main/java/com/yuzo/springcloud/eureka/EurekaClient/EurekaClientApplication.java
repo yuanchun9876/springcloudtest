7e0b3c219e745333f5b547ccbd6c837620085e62
yuanchun <yyuunn9876@163.com>
2019-03-09 11:38:51 +0800

    up EurekaClient

---
+++ EurekaClient/src/main/java/com/yuzo/springcloud/eureka/EurekaClient/EurekaClientApplication.java
@@ -0,0 +1,30 @@
+package com.yuzo.springcloud.eureka.EurekaClient;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableDiscoveryClient
+@RestController
+public class EurekaClientApplication {
+	
+	@Value("${server.port}")
+	private String port;
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaClientApplication.class, args);
+	}
+	
+	@RequestMapping("/hi")
+	public String home(@RequestParam(value="name", defaultValue="turing" )String name) {
+		return "hi!" + name + ", my port is " + port;
+	}
+
+}
